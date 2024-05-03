efa171f99bb0345b020222b88bd1a56138cfa812
Do Duy Dong <doduydong.97d@gmail.com>
2024-05-03 17:41:20 +0700

    Manage Test Data

--- testdata/afc/nopcommerce/data/UserData.java
+++ testdata/afc/nopcommerce/data/UserData.java
@@ -2,4 +2,18 @@ package afc.nopcommerce.data;
 
 public class UserData {
 
+	public static class UserInfo {
+
+		public static final String FIRSTNAME = "Dong";
+		public static final String LASTNAME = "Do";
+		public static final String EMAIL_USERNAME = "dongafc";
+		public static final String EMAIL_DOMAIN = "@gmail.com";
+		public static final String PASSWORD = "Selenium3@";
+		public static final String COMPANY = "AFC";
+		public static final String DOB_DAY = "13";
+		public static final String DOB_MONTH = "October";
+		public static final String DOB_YEAR = "1997";
+
+	}
+
 }
