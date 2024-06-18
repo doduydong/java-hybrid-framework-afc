d7e9eee0c1f3872381ff4393817dc1993439a5b0
Do Duy Dong <doduydong.97d@gmail.com>
2024-06-18 12:54:48 +0700

    Update - ExtentReports

--- actions/reportConfig/ExtentReportsListener.java
+++ actions/reportConfig/ExtentReportsListener.java
@@ -1,6 +1,10 @@
 package reportConfig;
 
-import static reportConfig.ExtentReportsManager.getTest;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -9,11 +13,47 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import commons.BaseTest;
+import commons.GlobalConstants;
 
 public class ExtentReportsListener implements ITestListener {
+	private static final ExtentReports extentReports = createExtentReports();
+	private static final Map<Integer, ExtentTest> extentTestMap = new ConcurrentHashMap<>();
+
+	private static ExtentReports createExtentReports() {
+		ExtentReports extentReports = new ExtentReports();
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(GlobalConstants.EXTENT_REPORTS_OUTPUT);
+
+		sparkReporter.config().setReportName("ExtentReports - nopCommerce");
+		sparkReporter.config().setDocumentTitle("ExtentReports - nopCommerce");
+		sparkReporter.config().setTimelineEnabled(true);
+		sparkReporter.config().setEncoding("utf-8");
+		sparkReporter.config().setTheme(Theme.DARK);
+
+		extentReports.attachReporter(sparkReporter);
+		extentReports.setSystemInfo("Company", "AFC");
+		extentReports.setSystemInfo("Project", "nopCommerce");
+		extentReports.setSystemInfo("SDET", "Dong");
+		extentReports.setSystemInfo("JDK Version", GlobalConstants.JDK_VERSION);
+
+		return extentReports;
+	}
+
+	public static synchronized ExtentTest startTest(String testName, String stepDesc) {
+		ExtentTest test = extentReports.createTest(testName, stepDesc);
+		extentTestMap.put((int) Thread.currentThread().getId(), test);
+		return test;
+	}
+
+	public static synchronized ExtentTest getTest() {
+		return extentTestMap.get((int) Thread.currentThread().getId());
+	}
 
 	@Override
 	public void onTestStart(ITestResult result) {
@@ -23,7 +63,7 @@ public class ExtentReportsListener implements ITestListener {
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		getTest().log(Status.PASS, "PASSED");
+		getTest().log(Status.PASS, result.getName() + " [PASSED]");
 	}
 
 	@Override
@@ -31,12 +71,12 @@ public class ExtentReportsListener implements ITestListener {
 		Object testInstance = result.getInstance();
 		WebDriver driver = ((BaseTest) testInstance).getDriver();
 		String screenshotBase64 = "data:image/png;base64," + ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-		getTest().log(Status.FAIL, "FAILED", getTest().addScreenCaptureFromBase64String(screenshotBase64).getModel().getMedia().get(0));
+		getTest().log(Status.FAIL, result.getThrowable().getMessage(), getTest().addScreenCaptureFromBase64String(screenshotBase64).getModel().getMedia().get(0));
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		getTest().log(Status.SKIP, "SKIPPED");
+		getTest().log(Status.SKIP, result.getThrowable().getMessage());
 	}
 
 	@Override
@@ -53,7 +93,13 @@ public class ExtentReportsListener implements ITestListener {
 
 	@Override
 	public void onFinish(ITestContext context) {
-		ExtentReportsManager.extentReports.flush();
+		extentReports.flush();
+		File extentReport = new File(GlobalConstants.EXTENT_REPORTS_OUTPUT);
+		try {
+			Desktop.getDesktop().browse(extentReport.toURI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- testcases/afc/nopcommerce/user/FW_17_Data_JSON_File.java
+++ testcases/afc/nopcommerce/user/FW_17_Data_JSON_File.java
@@ -18,7 +18,7 @@ import pageObjects.nopCommerce.CustomerInfoPageObject;
 import pageObjects.nopCommerce.HomePageObject;
 import pageObjects.nopCommerce.LoginPageObject;
 import pageObjects.nopCommerce.RegisterPageObject;
-import reportConfig.ExtentReportsManager;
+import reportConfig.ExtentReportsListener;
 
 public class FW_17_Data_JSON_File extends BaseTest {
 	private WebDriver driver;
@@ -49,122 +49,122 @@ public class FW_17_Data_JSON_File extends BaseTest {
 
 	@Test
 	public void User_01_Register(Method method) {
-		ExtentReportsManager.startTest(method.getName(), "User_01_Register");
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 01: Click 'Register' header link");
+		ExtentReportsListener.startTest(method.getName(), "User_01_Register");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 01: Click 'Register' header link");
 		registerPage = (RegisterPageObject) homePage.clickHeaderLinkByLinkText("Register");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 02: Check 'Male' radio button");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 02: Check 'Male' radio button");
 		registerPage.checkRadioButtonByLabel("Male");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 03: Enter '" + firstName + "' into 'FirstName' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 03: Enter '" + firstName + "' into 'FirstName' textbox");
 		registerPage.sendKeysToTextboxByID("FirstName", firstName);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 04: Enter '" + lastName + "' into 'LastName' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 04: Enter '" + lastName + "' into 'LastName' textbox");
 		registerPage.sendKeysToTextboxByID("LastName", lastName);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 05: Select '" + date + "' from 'Day' dropdown");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 05: Select '" + date + "' from 'Day' dropdown");
 		registerPage.selectDropdownByName("DateOfBirthDay", date);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 06: Select '" + month + "' from 'Month' dropdown");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 06: Select '" + month + "' from 'Month' dropdown");
 		registerPage.selectDropdownByName("DateOfBirthMonth", month);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 07: Select '" + year + "' from 'Year' dropdown");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 07: Select '" + year + "' from 'Year' dropdown");
 		registerPage.selectDropdownByName("DateOfBirthYear", year);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 08: Enter '" + emailAddress + "' into 'Email' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 08: Enter '" + emailAddress + "' into 'Email' textbox");
 		registerPage.sendKeysToTextboxByID("Email", emailAddress);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 09: Enter '" + company + "' into 'Company' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 09: Enter '" + company + "' into 'Company' textbox");
 		registerPage.sendKeysToTextboxByID("Company", company);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 10: Check 'Newsletter' checkbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 10: Check 'Newsletter' checkbox");
 		registerPage.checkCheckboxByID("Newsletter");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 11: Enter '" + password + "' into 'Password' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 11: Enter '" + password + "' into 'Password' textbox");
 		registerPage.sendKeysToTextboxByID("Password", password);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 12: Enter '" + password + "' into 'ConfirmPassword' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 12: Enter '" + password + "' into 'ConfirmPassword' textbox");
 		registerPage.sendKeysToTextboxByID("ConfirmPassword", password);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 13: Click 'Register' button");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 13: Click 'Register' button");
 		registerPage.clickRegisterButton();
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 14: Verify 'Your registration completed' message is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 14: Verify 'Your registration completed' message is displayed");
 		Assert.assertEquals(registerPage.getRegisterSuccessMessage(), "Your registration completed");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_01_Register - Step 15: Click 'Continue' button");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_01_Register - Step 15: Click 'Continue' button");
 		homePage = registerPage.clickContinueButton();
 	}
 
 	@Test
 	public void User_02_Login(Method method) {
-		ExtentReportsManager.startTest(method.getName(), "User_02_Login");
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 01: Click 'Log in' header link");
+		ExtentReportsListener.startTest(method.getName(), "User_02_Login");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 01: Click 'Log in' header link");
 		loginPage = (LoginPageObject) homePage.clickHeaderLinkByLinkText("Log in");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 02: Enter '" + emailAddress + "' into 'Email' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 02: Enter '" + emailAddress + "' into 'Email' textbox");
 		loginPage.sendKeysToTextboxByID("Email", emailAddress);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 03: Enter '" + password + "' into 'Password' textbox");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 03: Enter '" + password + "' into 'Password' textbox");
 		loginPage.sendKeysToTextboxByID("Password", password);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 04: Click 'Log in' button");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 04: Click 'Log in' button");
 		homePage = loginPage.clickLoginButton();
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 05: Verify 'My account' header link is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 05: Verify 'My account' header link is displayed");
 		Assert.assertTrue(homePage.isHeaderLinkDisplayedByLinkText("My account"));
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_02_Login - Step 06: Verify 'Log out' header link is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_02_Login - Step 06: Verify 'Log out' header link is displayed");
 		Assert.assertTrue(homePage.isHeaderLinkDisplayedByLinkText("Log out"));
 	}
 
 	@Test
 	public void User_03_Customer_Info(Method method) {
-		ExtentReportsManager.startTest(method.getName(), "User_03_Customer_Info");
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 01: Click 'My account' header link");
+		ExtentReportsListener.startTest(method.getName(), "User_03_Customer_Info");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 01: Click 'My account' header link");
 		customerInfoPage = (CustomerInfoPageObject) homePage.clickHeaderLinkByLinkText("My account");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 02: Verify 'Log out' header link is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 02: Verify 'Log out' header link is displayed");
 		Assert.assertTrue(homePage.isHeaderLinkDisplayedByLinkText("Log out"));
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 03: Verify 'Male' radio button is selected");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 03: Verify 'Male' radio button is selected");
 		Assert.assertTrue(customerInfoPage.isRadioButtonSelectedByLabel("Male"));
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 04: Verify value of 'FirstName' textbox is '" + firstName + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 04: Verify value of 'FirstName' textbox is '" + firstName + "'");
 		Assert.assertEquals(customerInfoPage.getTextboxValueByID("FirstName"), firstName);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 05: Verify value of 'LastName' textbox is '" + lastName + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 05: Verify value of 'LastName' textbox is '" + lastName + "'");
 		Assert.assertEquals(customerInfoPage.getTextboxValueByID("LastName"), lastName);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 06: Verify value of 'Day' dropdown is '" + date + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 06: Verify value of 'Day' dropdown is '" + date + "'");
 		Assert.assertEquals(customerInfoPage.getDropdownValueByName("DateOfBirthDay"), date);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 07: Verify value of 'Month' dropdown is '" + month + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 07: Verify value of 'Month' dropdown is '" + month + "'");
 		Assert.assertEquals(customerInfoPage.getDropdownValueByName("DateOfBirthMonth"), month);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 08: Verify value of 'Year' dropdown is '" + year + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 08: Verify value of 'Year' dropdown is '" + year + "'");
 		Assert.assertEquals(customerInfoPage.getDropdownValueByName("DateOfBirthYear"), year);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 09: Verify value of 'Email' textbox is '" + emailAddress + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 09: Verify value of 'Email' textbox is '" + emailAddress + "'");
 		Assert.assertEquals(customerInfoPage.getTextboxValueByID("Email"), emailAddress);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 10: Verify value of 'Company' textbox is '" + company + "'");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 10: Verify value of 'Company' textbox is '" + company + "'");
 		Assert.assertEquals(customerInfoPage.getTextboxValueByID("Company"), company);
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_03_Customer_Info - Step 11: Verify 'Newsletter' checkbox is selected");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_03_Customer_Info - Step 11: Verify 'Newsletter' checkbox is selected");
 		Assert.assertTrue(customerInfoPage.isCheckboxSelectedByID("Newsletter"));
 	}
 
 	@Test
 	public void User_04_Logout(Method method) {
-		ExtentReportsManager.startTest(method.getName(), "User_04_Logout");
-		ExtentReportsManager.getTest().log(Status.INFO, "User_04_Logout - Step 01: Click 'Log out' header link");
+		ExtentReportsListener.startTest(method.getName(), "User_04_Logout");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_04_Logout - Step 01: Click 'Log out' header link");
 		homePage = (HomePageObject) customerInfoPage.clickHeaderLinkByLinkText("Log out");
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_04_Logout - Step 02: Verify 'Register' header link is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_04_Logout - Step 02: Verify 'Register' header link is displayed");
 		Assert.assertTrue(homePage.isHeaderLinkDisplayedByLinkText("Register"));
 
-		ExtentReportsManager.getTest().log(Status.INFO, "User_04_Logout - Step 03: Verify 'Log in' header link is displayed");
+		ExtentReportsListener.getTest().log(Status.INFO, "User_04_Logout - Step 03: Verify 'Log in' header link is displayed");
 		Assert.assertTrue(homePage.isHeaderLinkDisplayedByLinkText("Log in"));
 	}
 
